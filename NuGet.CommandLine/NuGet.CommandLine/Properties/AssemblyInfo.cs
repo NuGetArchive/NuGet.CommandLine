ef682826d387a3f3cc65cce874862a244958b2cd
zhili1208 <zhili128@outlook.com>
2015-01-06 11:35:53 -0800

    create NuGet.CommandLine project

---
+++ NuGet.CommandLine/NuGet.CommandLine/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGet.CommandLine")]
+[assembly: AssemblyDescription("NuGet Command Line Tool")]
+
